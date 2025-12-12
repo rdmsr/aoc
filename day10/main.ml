00db109e8fbc22582d6ebcf0a8ce1eaede15a2a7
lg <abbix@riseup.net>
2025-12-12 07:24:12 -0500

    format code

--- day10/main.ml
+++ day10/main.ml
@@ -4,36 +4,26 @@ open Z3
 let read_all_lines f : string list =
   In_channel.with_open_text f In_channel.input_lines
 
-type item =
-  | Lights of string
-  | Button of int list
-  | Joltage of int list
+type item = Lights of string | Button of int list | Joltage of int list
 
 let ws = skip_many (char ' ')
 
 let integer =
-  take_while1 (function '0' .. '9' -> true | _ -> false)
-  >>| int_of_string
+  take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
 
-let comma_sep p =
-  sep_by (char ',' *> ws) p
+let comma_sep p = sep_by (char ',' *> ws) p
 
 let lights =
-  char '[' *> take_till (fun c -> c = ']') <* char ']'
-  >>| fun s -> Lights s
+  char '[' *> take_till (fun c -> c = ']') <* char ']' >>| fun s -> Lights s
 
 let button =
-  char '(' *> ws *> comma_sep integer <* ws <* char ')'
-  >>| fun xs -> Button xs
+  char '(' *> ws *> comma_sep integer <* ws <* char ')' >>| fun xs -> Button xs
 
 let joltage =
-  char '{' *> ws *> comma_sep integer <* ws <* char '}'
-  >>| fun xs -> Joltage xs
+  char '{' *> ws *> comma_sep integer <* ws <* char '}' >>| fun xs -> Joltage xs
 
 let item = lights <|> button <|> joltage
-
-let items =
-  ws *> many (item <* ws)
+let items = ws *> many (item <* ws)
 
 let parse_line s =
   match parse_string ~consume:Consume.All items s with
@@ -41,100 +31,101 @@ let parse_line s =
   | Error msg -> failwith msg
 
 let light_to_mask s =
-  String.fold_left (fun acc c ->
-    match c with
-    | '.' -> (acc lsl 1)
-    | '#' -> (acc lsl 1) lor 1
-    | _ -> acc
-  ) 0 s
+  String.fold_left
+    (fun acc c ->
+      match c with '.' -> acc lsl 1 | '#' -> (acc lsl 1) lor 1 | _ -> acc)
+    0 s
 
 let rec combinations k lst =
   if k = 0 then [ [] ]
   else
-	match lst with
-	| [] -> []
-	| x :: xs ->
-		let with_x = List.map (fun rest -> x :: rest) (combinations (k-1) xs) in
-		let without_x = combinations k xs in
-		with_x @ without_x
+    match lst with
+    | [] -> []
+    | x :: xs ->
+        let with_x =
+          List.map (fun rest -> x :: rest) (combinations (k - 1) xs)
+        in
+        let without_x = combinations k xs in
+        with_x @ without_x
 
 let rec mask_from_button mask len = function
   | [] -> mask
   | x :: xs ->
-	  let new_mask = (mask lor (1 lsl (len - 1 - x))) in mask_from_button new_mask len xs
+      let new_mask = mask lor (1 lsl (len - 1 - x)) in
+      mask_from_button new_mask len xs
 
-
-let part1 lines = 
+let part1 lines =
   let parsed = List.map parse_line lines in
 
-  List.fold_left (fun acc line ->
-		let light = line |> List.find (function Lights _ -> true | _ -> false) in
-		let (len, light_mask) =  
-		  match light with
-		  | Lights s -> (String.length s, light_to_mask s)
-		  | _ -> (0, 0)
-		in
+  List.fold_left
+    (fun acc line ->
+      let light =
+        line |> List.find (function Lights _ -> true | _ -> false)
+      in
+      let len, light_mask =
+        match light with
+        | Lights s -> (String.length s, light_to_mask s)
+        | _ -> (0, 0)
+      in
 
-		let all_buttons = 
-		  List.fold_right (fun item acc ->
-				match item with
-				| Button b -> (mask_from_button 0 len b) :: acc 
-				| _ -> acc            
-			) line []
-
-		in
-		let find_presses buttons =
-		  let rec try_size k =
-			let combos = combinations k buttons
-			in match List.find_opt
-					   (fun combo ->
-						   (List.fold_left
-							  (lxor) 0 combo) = light_mask
-					   ) combos
-			   with
-			   | Some combo -> List.length combo
-			   | None ->
-				   if k > List.length buttons then
-					failwith "No combination found"
-				   else
-					 try_size (k+1)
-		  in try_size 1
-		in acc + find_presses all_buttons) 0 parsed
-
+      let all_buttons =
+        List.fold_right
+          (fun item acc ->
+            match item with
+            | Button b -> mask_from_button 0 len b :: acc
+            | _ -> acc)
+          line []
+      in
+      let find_presses buttons =
+        let rec try_size k =
+          let combos = combinations k buttons in
+          match
+            List.find_opt
+              (fun combo -> List.fold_left ( lxor ) 0 combo = light_mask)
+              combos
+          with
+          | Some combo -> List.length combo
+          | None ->
+              if k > List.length buttons then failwith "No combination found"
+              else try_size (k + 1)
+        in
+        try_size 1
+      in
+      acc + find_presses all_buttons)
+    0 parsed
 
 let solve_machine buttons target =
   let ctx = mk_context [] in
   let opt = Optimize.mk_opt ctx in
 
-  let m = Array.length buttons in     
+  let m = Array.length buttons in
   let n = Array.length target in
 
   let xs =
     Array.init m (fun j ->
-		  (* Make a variable x_{i} such that x_{i} is >= 0 for every button *)
-		  let value = Arithmetic.Integer.mk_const_s ctx ("x" ^ string_of_int j) in
-		  let zero = Arithmetic.Integer.mk_numeral_i ctx 0 in
-		  Optimize.add opt [Arithmetic.mk_ge ctx value zero];
-		  value
-    )
+        (* Make a variable x_{i} such that x_{i} is >= 0 for every button *)
+        let value = Arithmetic.Integer.mk_const_s ctx ("x" ^ string_of_int j) in
+        let zero = Arithmetic.Integer.mk_numeral_i ctx 0 in
+        Optimize.add opt [ Arithmetic.mk_ge ctx value zero ];
+        value)
   in
 
   for i = 0 to n - 1 do
-	(* Find all buttons affecting this joltage level *)
+    (* Find all buttons affecting this joltage level *)
     let affecting =
-      List.filter_map (fun j ->
-        if List.mem i buttons.(j) then Some xs.(j) else None
-      ) (List.init m (fun j -> j))
+      List.filter_map
+        (fun j -> if List.mem i buttons.(j) then Some xs.(j) else None)
+        (List.init m (fun j -> j))
     in
 
     let sum =
       match affecting with
       | [] -> Arithmetic.Integer.mk_numeral_i ctx 0
-      | _  -> Arithmetic.mk_add ctx affecting
+      | _ -> Arithmetic.mk_add ctx affecting
     in
-	(* Sum of all affecting button presses = desired joltage level *)
+    (* Sum of all affecting button presses = desired joltage level *)
     let rhs = Arithmetic.Integer.mk_numeral_i ctx target.(i) in
-    Optimize.add opt [Boolean.mk_eq ctx sum rhs];
+    Optimize.add opt [ Boolean.mk_eq ctx sum rhs ]
   done;
 
   (* Minimize the sum of all variables (button presses) *)
@@ -142,41 +133,39 @@ let solve_machine buttons target =
   ignore (Optimize.minimize opt sum);
 
   match Optimize.check opt with
-  | Solver.SATISFIABLE ->
-	  (let model = match Optimize.get_model opt with
-			| Some a -> a
-			| None -> failwith "wtf"
-	   in match (Model.eval model sum true) with
-		  | Some v -> int_of_string (Expr.to_string v)
-		  | _ -> 0)
+  | Solver.SATISFIABLE -> (
+      let model =
+        match Optimize.get_model opt with Some a -> a | None -> failwith "wtf"
+      in
+      match Model.eval model sum true with
+      | Some v -> int_of_string (Expr.to_string v)
+      | _ -> 0)
+  | _ -> failwith "No solution"
 
-  | _ ->
-      failwith "No solution"
-
-
-let part2 lines = 
+let part2 lines =
   let parsed = List.map parse_line lines in
 
-  List.fold_left (fun acc line ->
-		let joltage_item = line |> List.find (function Joltage _ -> true | _ -> false) in
+  List.fold_left
+    (fun acc line ->
+      let joltage_item =
+        line |> List.find (function Joltage _ -> true | _ -> false)
+      in
 
-		let joltage = match joltage_item with
-			| Joltage a -> a
-			|_ -> failwith "wtf"
-		in
+      let joltage =
+        match joltage_item with Joltage a -> a | _ -> failwith "wtf"
+      in
 
-		let all_buttons = 
-		  List.fold_right (fun item acc ->
-				match item with
-				| Button b -> b :: acc 
-				| _ -> acc            
-			) line []
-
-		in acc + solve_machine (Array.of_list all_buttons) (Array.of_list joltage)) 0 parsed
+      let all_buttons =
+        List.fold_right
+          (fun item acc -> match item with Button b -> b :: acc | _ -> acc)
+          line []
+      in
+      acc + solve_machine (Array.of_list all_buttons) (Array.of_list joltage))
+    0 parsed
 
 let () =
   let lines = read_all_lines Sys.argv.(1) in
   let result1 = part1 lines in
   let result2 = part2 lines in
   Printf.printf "1: %d\n" result1;
-  Printf.printf "2: %d\n" result2;
+  Printf.printf "2: %d\n" result2
--- day8/fast.ml
+++ day8/fast.ml
@@ -9,10 +9,10 @@ let distance (x, y, z) (x', y', z') =
 let parse_coord s = Scanf.sscanf s "%d,%d,%d" (fun a b c -> (a, b, c))
 
 module Coord = struct
-	type t = int * int * int
+  type t = int * int * int
 
-	let compare = compare
-  end
+  let compare = compare
+end
 
 module CoordSet = Set.Make (Coord)
 
@@ -20,10 +20,9 @@ let find_set elem arr =
   let rec aux i =
     if i = Array.length arr then None
     else if CoordSet.mem elem arr.(i) then Some (i, arr.(i))
-		 else aux (i + 1)
+    else aux (i + 1)
   in
   match aux 0 with Some x -> x | None -> failwith "wtf"
-
 
 let for_each_edge coords f =
   let rec aux xs =
@@ -40,23 +39,20 @@ let rec make_coords acc = function
   | line :: rest -> make_coords (parse_coord line :: acc) rest
 
 let insert_in_heap heap n elem cmp =
-  if Pairing_heap.length heap < n then
-    Pairing_heap.add heap elem
+  if Pairing_heap.length heap < n then Pairing_heap.add heap elem
   else begin
-		 match Pairing_heap.top heap with
-		 | Some top ->
-			 if cmp elem top < 0 then begin
-										Pairing_heap.remove_top heap;
-										Pairing_heap.add heap elem
-									  end
-		 | None -> Pairing_heap.add heap elem
-	   end
+    match Pairing_heap.top heap with
+    | Some top ->
+        if cmp elem top < 0 then begin
+          Pairing_heap.remove_top heap;
+          Pairing_heap.add heap elem
+        end
+    | None -> Pairing_heap.add heap elem
+  end
 
 let heap_ordered_list heap =
   let rec aux acc =
-    match Pairing_heap.pop heap with
-    | None -> acc
-    | Some x -> aux (x :: acc)
+    match Pairing_heap.pop heap with None -> acc | Some x -> aux (x :: acc)
   in
   aux []
 
@@ -65,69 +61,71 @@ let part1 lines n =
   let coords_arr = Array.of_list coords in
   let arr =
     Array.init (List.length coords) (fun i ->
-          CoordSet.empty |> CoordSet.add coords_arr.(i))
+        CoordSet.empty |> CoordSet.add coords_arr.(i))
   in
 
   let rec connect_edges i arr = function
-      | [] -> arr
-      | (x1, x2) :: xs ->
-          if i = 0 then arr
-          else
-			let iset1, set1 = find_set x1 arr in
-			let iset2, set2 = find_set x2 arr in
-			if iset1 <> iset2 then begin
-									 arr.(iset1) <- CoordSet.union set1 set2;
-									 arr.(iset2) <- CoordSet.empty
-								   end;
-			connect_edges (i - 1) arr xs
+    | [] -> arr
+    | (x1, x2) :: xs ->
+        if i = 0 then arr
+        else
+          let iset1, set1 = find_set x1 arr in
+          let iset2, set2 = find_set x2 arr in
+          if iset1 <> iset2 then begin
+            arr.(iset1) <- CoordSet.union set1 set2;
+            arr.(iset2) <- CoordSet.empty
+          end;
+          connect_edges (i - 1) arr xs
   in
-  let heap = Pairing_heap.create ~min_size:n ~cmp:(fun (a, b) (c, d) -> distance c d - distance a b) () in
+  let heap =
+    Pairing_heap.create ~min_size:n
+      ~cmp:(fun (a, b) (c, d) -> distance c d - distance a b)
+      ()
+  in
 
   for_each_edge coords (fun x elem ->
-    insert_in_heap heap n (x, elem) (fun (a, b) (c, d) -> distance a b - distance c d));
+      insert_in_heap heap n (x, elem) (fun (a, b) (c, d) ->
+          distance a b - distance c d));
 
-  heap_ordered_list heap
-	|> connect_edges n arr
-	|> Array.sort (fun a b -> compare (CoordSet.cardinal b) (CoordSet.cardinal a));
+  heap_ordered_list heap |> connect_edges n arr
+  |> Array.sort (fun a b -> compare (CoordSet.cardinal b) (CoordSet.cardinal a));
 
   CoordSet.cardinal arr.(0)
-	* CoordSet.cardinal arr.(1)
-	* CoordSet.cardinal arr.(2)
+  * CoordSet.cardinal arr.(1)
+  * CoordSet.cardinal arr.(2)
 
 let part2 lines =
   let coords = make_coords [] lines in
   let coords_arr = Array.of_list coords in
   let n = Array.length coords_arr in
   let arr =
-	Array.init n (fun i ->
-		  CoordSet.empty |> CoordSet.add coords_arr.(i))
+    Array.init n (fun i -> CoordSet.empty |> CoordSet.add coords_arr.(i))
   in
 
   let rec connect_edges i acc heap =
-	if i = 1 then acc else
-	  match Pairing_heap.pop heap with
-	  | None -> acc
-	  | Some(e1, e2) ->
-		  let x1, _, _ = e1 in
-		  let x2, _, _ = e2 in
-		  let iset1, set1 = find_set e1 arr in
-		  let iset2, set2 = find_set e2 arr in
+    if i = 1 then acc
+    else
+      match Pairing_heap.pop heap with
+      | None -> acc
+      | Some (e1, e2) ->
+          let x1, _, _ = e1 in
+          let x2, _, _ = e2 in
+          let iset1, set1 = find_set e1 arr in
+          let iset2, set2 = find_set e2 arr in
           if iset1 <> iset2 then begin
-								   arr.(iset1) <- CoordSet.union set1 set2;
-								   arr.(iset2) <- CoordSet.empty;
-								   connect_edges (i - 1) (x1 * x2) heap
-								 end
-          else 
-			connect_edges i acc heap
+            arr.(iset1) <- CoordSet.union set1 set2;
+            arr.(iset2) <- CoordSet.empty;
+            connect_edges (i - 1) (x1 * x2) heap
+          end
+          else connect_edges i acc heap
   in
-  let heap = Pairing_heap.create 
-			   ~cmp:(fun (a, b) (c, d) -> distance a b - distance c d) 
-			   () 
+  let heap =
+    Pairing_heap.create
+      ~cmp:(fun (a, b) (c, d) -> distance a b - distance c d)
+      ()
   in
-  for_each_edge coords (fun x elem ->
-				Pairing_heap.add heap (x, elem));
+  for_each_edge coords (fun x elem -> Pairing_heap.add heap (x, elem));
 
- 
   connect_edges n 0 heap
 
 let () =
